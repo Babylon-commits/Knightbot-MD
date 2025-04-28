6cc5e10ad8f73cd03cf7c95fc0810e530e0e823b
Babylon-commits <djeremiah687@gmail.com>
2025-04-28 17:00:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,11 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: ' 
+
+⸸ BΛB¥LØN
+', // Your name
+  ownerNumber: '2349033423786', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
